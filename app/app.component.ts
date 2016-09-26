06e6fd7245e951c18fccc8671bc679f6b2ee5c4e
Vinay Pandya <005vinay@gmail.com>
2016-09-26 11:48:29 +0530

    quick start of angular-tour-of-heroes

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by vinay on 9/26/2016.
+ */
+
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent { }
